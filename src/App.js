dad77afc46855d33a3a17ef2d8e162d0533cec18
Yurii Zubar <barsuk0831@gmail.com>
2020-12-13 19:08:03 +0200

    end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { ToastContainer } from "react-toastify";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Searchbar from "./components/Searchbar";
 import ImageGallery from "./components/ImageGallery";
 import ButtonLoadMore from "./components/Button";
+import Modal from "./components/Modal";
 import imageApi from "./services/image-api";
 import "./App.css";
 
@@ -12,6 +15,8 @@ class App extends Component {
     images: [],
     currentPage: 1,
     imageQuery: "",
+    showModal: false,
+    largeImage: "",
     isLoading: false,
     error: null,
   };
@@ -28,32 +33,72 @@ class App extends Component {
     }
   }
 
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  onClickImage = (e) => {
+    e.preventDefault();
+    if (e.target.nodeName === "IMG") {
+      this.toggleModal();
+      this.setState({ largeImage: e.target.dataset.image });
+    }
+  };
+
   handleFormSubmit = (imageQuery) => {
     this.setState({ imageQuery, currentPage: 1, images: [] });
   };
 
   fetchImage = () => {
-    const { currentPage, imageQuery } = this.state;
+    const { currentPage, imageQuery, isLoading } = this.state;
+
     const options = {
       currentPage,
       imageQuery,
     };
-
-    imageApi.fetchImage(options).then(({ hits }) =>
-      this.setState((prevState) => ({
-        images: [...prevState.images, ...hits],
-        currentPage: prevState.currentPage + 1,
-      }))
-    );
+    this.setState({ isLoading: true });
+    imageApi
+      .fetchImage(options)
+      .then(({ hits }) =>
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...hits],
+          currentPage: prevState.currentPage + 1,
+        }))
+      )
+      .then(() => {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: "smooth",
+        });
+      })
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ isLoading: false }));
   };
 
   render() {
-    const { images } = this.state;
+    const { images, showModal, isLoading } = this.state;
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery images={images} />
+        <ImageGallery images={images} onClick={this.onClickImage} />
+        <div className="spinner">
+          {isLoading && (
+            <Loader
+              type="BallTriangle"
+              color="#00BFFF"
+              height={80}
+              width={80}
+            />
+          )}
+        </div>
         {images.length > 0 && <ButtonLoadMore onClick={this.fetchImage} />}
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={this.state.largeImage} alt="modal" />
+          </Modal>
+        )}
         <ToastContainer autoClose={2000} />
       </div>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,9 @@
 import ImageGalleryItem from "../ImageGalleryItem";
 import s from "./ImageGallery.module.css";
 
-function ImageGallery({ images }) {
+function ImageGallery({ images, onClick }) {
   return (
-    <ul className={s.ImageGallery}>
+    <ul className={s.ImageGallery} onClick={onClick}>
       <ImageGalleryItem images={images} />
     </ul>
   );
